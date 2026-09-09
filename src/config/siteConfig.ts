11e2fa123020c7d90ab7237ad8b0ba02f0aaff32
N0rmal7 <N0rmal7@users.noreply.github.com>
2026-09-09 23:30:06 +0800

    fix: set site runtime start date to 2026-05-01

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -123,7 +123,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2026-09-06T22:41:49+08:00",
+	siteStartDate: "2026-05-01T00:00:00+08:00",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
